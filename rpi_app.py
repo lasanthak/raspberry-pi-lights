3e5088878545196ef885a453caf2773bf671e124
Lasantha Kularatne <lkularatne@homeaway.com>
2016-10-27 16:36:41 -0500

    cmd line params

--- rpi_app.py
+++ rpi_app.py
@@ -1,3 +1,4 @@
+import sys
 from gpiozero import OutputDevice
 from time import sleep
 from pin_patterns import PatternAlgorithm
@@ -12,6 +13,17 @@ pins = [
     OutputDevice(pin=25, active_high=False),
     OutputDevice(pin=24, active_high=False)
 ]
+
+# ------------------------------------
+# set or reset all pins of user give ON/OFF params
+if len(sys.argv) > 1:
+    if "on" == sys.argv[1].lower():
+        for p in pins:
+            p.value = True
+    elif "off" == sys.argv[1].lower():
+        for p in pins:
+            p.value = False
+# ------------------------------------
 
 pattern_algorithm = PatternAlgorithm(10)
 
